9f405988e944fdafcf741b6ae34f960acde9a7ee
kazumi <kazumi121589@yahoo.co.jp>
2022-02-11 23:27:46 +0900

    Modified calendar

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,34 +9,25 @@ opt.on("-m VAL") { |v| params[:m] = v }
 
 opt.parse!(ARGV)
 
-if params[:y] == nil
-  a = Date.today.year
-else
-  a = params[:y].to_i
-end
-if params[:m] == nil
-  b = Date.today.month
-else
-  b = params[:m].to_i
-end
+year = params[:y]&.to_i || Date.today.year
+month = params[:m]&.to_i || Date.today.month
 
-puts Date.new(a, b).strftime("%m月 %Y").center(20)
+puts Date.new(year, month).strftime("%m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
 
-firstday = Date.new(a, b, 1).strftime("%w")
-f = firstday.to_i
-f.times do
+firstday = Date.new(year, month, 1).strftime("%w")
+fday = firstday.to_i
+fday.times do
   print "   "
 end
 
-days = (Date.new(a, b, 1)..Date.new(a, b, -1))
+days = (Date.new(year, month, 1)..Date.new(year, month, -1))
 days.each do |day|
-  a = day.strftime("%e ")
-  print a
+  all_days = day.strftime("%e ")
+  print all_days
   if day.wday == 6
     puts "\n"
   end
 end
 
 puts "\n"
-
