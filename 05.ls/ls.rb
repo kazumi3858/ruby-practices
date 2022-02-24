db1d96951884f9bb635d09d3330af6eee9a4c205
kazumi <kazumi121589@yahoo.co.jp>
2022-02-24 11:23:20 +0900

    Modified ls program

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
+TEXT_WIDTH = 30
+
 def file_lists
   Dir.glob('*').map do |list|
-    list.ljust(30)
+    list.ljust(TEXT_WIDTH)
   end
 end
 
 def create_columns
+  lists = file_lists
   columns = 3
-  case file_lists.size % columns
-  when 0
-    file_lists.each_slice(file_lists.size / columns).to_a
-  when 1
-    file_lists.push(nil, nil).each_slice((file_lists.size + 2) / columns).to_a
-  when 2
-    file_lists.push(nil).each_slice((file_lists.size + 1) / columns).to_a
-  end
+  rest_of_row = lists.size % columns
+  (columns - rest_of_row).times { lists.push(nil) } if rest_of_row != 0
+  lists.each_slice(lists.size / columns).to_a
 end
 
 def show_results
