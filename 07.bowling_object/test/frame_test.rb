3d5eb771bcf4e12e05fe6d0aca3d6175456e4035
kazumi <kazumi121589@yahoo.co.jp>
2022-04-19 12:43:46 +0900

    Modified test files

--- 07.bowling_object/test/frame_test.rb
+++ 07.bowling_object/test/frame_test.rb
@@ -5,7 +5,7 @@ require_relative '../lib/frame'
 
 class FrameTest < Minitest::Test
   def test_frame
-    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10, 0], [9, 1], [8, 0], [10, 0], [6, 4, 5]],
+    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10], [6, 4, 5]],
                  Frame.new.create_frames('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
   end
 end
--- 07.bowling_object/test/game_test.rb
+++ 07.bowling_object/test/game_test.rb
@@ -4,11 +4,11 @@ require 'minitest/autorun'
 require_relative '../lib/game'
 
 class GameTest < Minitest::Test
-  def test_total_score_when_index_8_and_next_are_consecutive_strike
-    assert_equal 164, Game.new.show_total_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X') # test numbers contains consecutive strike, normal strike, spare
+  def test_total_score_when_ninth_frame_and_next_shot_are_consecutive_strike
+    assert_equal 164, Game.new.show_total_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
   end
 
-  def test_total_score_when_index_8_and_next_are_not_consecutive_strike
+  def test_total_score_when_ninth_frame_and_next_shot_are_not_consecutive_strike
     assert_equal 139, Game.new.show_total_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
   end
 end
