9a322d0c9384b213648876ca44ad2bb0cbbf28ce
kazumi <kazumi121589@yahoo.co.jp>
2022-04-27 20:14:03 +0900

    Modified game and frame methods

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -6,12 +6,31 @@ class Frame
   attr_reader :first_shot, :second_shot, :third_shot
 
   def initialize(first_shot, second_shot, third_shot = nil)
-    @first_shot = Shot.new(first_shot).score
-    @second_shot = Shot.new(second_shot).score
-    @third_shot = Shot.new(third_shot).score
+    @first_shot = Shot.new(first_shot)
+    @second_shot = Shot.new(second_shot)
+    @third_shot = Shot.new(third_shot)
   end
 
   def score
-    [first_shot, second_shot, third_shot].sum
+    [first_shot.score, second_shot.score, third_shot.score].sum
+  end
+
+  def strike
+    first_shot.score == 10
+  end
+
+  def spare
+    score == 10
+  end
+
+  def self.frames(numbers)
+    numbers = numbers.split(',')
+    numbers.map.with_index do |number, index|
+      numbers.insert(index + 1, nil) if number == 'X'
+    end
+    groups_of_numbers = numbers.first(18).each_slice(2).to_a.push(numbers[18..]).map(&:compact)
+    groups_of_numbers.map do |group|
+      Frame.new(group[0], group[1], group[2])
+    end
   end
 end
--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -3,33 +3,27 @@
 require_relative 'frame'
 
 class Game
-  def show_total_score(numbers)
-    frames = frames(numbers)
+  def initialize(numbers)
+    @frames = Frame.frames(numbers)
+  end
+
+  def show_total_score
     total_score = 0
-    frames.first(9).each_with_index do |frame, index|
+    @frames.first(9).each_with_index do |frame, index|
       total_score += frame.score
-      total_score += bonus_scores(frames, frame, index)
+      total_score += bonus_scores(@frames, frame, index)
     end
-    total_score += frames[9].score
+    total_score += @frames[9].score
   end
 
-  def frames(numbers)
-    numbers = numbers.gsub('X', '10,NA').split(',')
-    divided_numbers = numbers.first(18).each_slice(2).to_a.push(numbers[18..])
-    divided_numbers.map do |shots|
-      shots.delete('NA') if shots.include?('NA')
-      Frame.new(shots[0], shots[1], shots[2])
-    end
-  end
-
-  def bonus_scores(frames, frame, index)
-    next_frame = frames[index + 1]
-    after_next_frame = frames[index + 2]
-    if frame.first_shot == 10 && next_frame.first_shot == 10
+  def bonus_scores(frame, index)
+    next_frame = @frames[index + 1]
+    after_next_frame = @frames[index + 2]
+    if frame.strike && next_frame.strike
       consecutive_strike_score(index, next_frame, after_next_frame)
-    elsif frame.first_shot == 10
+    elsif frame.strike
       strike_score(next_frame)
-    elsif frame.score == 10
+    elsif frame.spare
       spare_score(next_frame)
     else
       0
@@ -37,17 +31,17 @@ class Game
   end
 
   def consecutive_strike_score(index, next_frame, after_next_frame)
-    ninth_frame = index == 8
-    ninth_frame ? 10 + next_frame.second_shot : 10 + after_next_frame.first_shot
+    10 + (index == 8 ? next_frame.second_shot.score : after_next_frame.first_shot.score)
   end
 
   def strike_score(next_frame)
-    next_frame.first_shot + next_frame.second_shot
+    next_frame.first_shot.score + next_frame.second_shot.score
   end
 
   def spare_score(next_frame)
-    next_frame.first_shot
+    next_frame.first_shot.score
   end
 end
 
-puts Game.new.show_total_score(ARGV[0])
+game = Game.new(ARGV[0])
+puts game.show_total_score
