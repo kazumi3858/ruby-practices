328a935a792616e588f233b66921bf3082ff4427
kazumi <kazumi121589@yahoo.co.jp>
2022-05-13 11:03:15 +0900

    Replaced a variable name file_list with file_data_list

--- 08.ls_object/lib/long_file_list.rb
+++ 08.ls_object/lib/long_file_list.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
 class LongFileList
-  def initialize(file_list)
-    @file_list = file_list
+  def initialize(file_data_list)
+    @file_data_list = file_data_list
   end
 
   def show_list
     max_length_hash = calc_max_length
-    formated_list = @file_list.map { |file| format_list(file, max_length_hash).join }
-    ["total #{calculate_total_block_number}", *formated_list]
+    formated_file_data_list = @file_data_list.map { |file_data| format_list(file_data, max_length_hash).join }
+    ["total #{calculate_total_block_number}", *formated_file_data_list]
   end
 
   private
 
   def calculate_total_block_number
     # デフォルドでは1ブロックが1024バイト。これを2で割ってlsコマンドの標準512バイトに合わせる。
-    @file_list.sum(&:blocks) / 2
+    @file_data_list.sum(&:blocks) / 2
   end
 
   def calc_max_length
     {
-      nlink: @file_list.map { |file| file.nlink.to_s.length }.max + 1,
-      uid: @file_list.map { |file| file.uid.length }.max + 1,
-      gid: @file_list.map { |file| file.gid.length }.max + 1,
-      size: @file_list.map { |file| file.size.to_s.length }.max + 1
+      nlink: @file_data_list.map { |file_data| file_data.nlink.to_s.length }.max + 1,
+      uid: @file_data_list.map { |file_data| file_data.uid.length }.max + 1,
+      gid: @file_data_list.map { |file_data| file_data.gid.length }.max + 1,
+      size: @file_data_list.map { |file_data| file_data.size.to_s.length }.max + 1
     }
   end
 
-  def format_list(file, max_length_hash)
+  def format_list(file_data, max_length_hash)
     [
-      file.type,
-      file.permission,
-      file.nlink.to_s.rjust(max_length_hash[:nlink]),
-      file.uid.rjust(max_length_hash[:uid]),
-      file.gid.rjust(max_length_hash[:gid]),
-      file.size.to_s.rjust(max_length_hash[:size]),
-      file.time.strftime('%b %e %R ').rjust(14),
-      file.name
+      file_data.type,
+      file_data.permission,
+      file_data.nlink.to_s.rjust(max_length_hash[:nlink]),
+      file_data.uid.rjust(max_length_hash[:uid]),
+      file_data.gid.rjust(max_length_hash[:gid]),
+      file_data.size.to_s.rjust(max_length_hash[:size]),
+      file_data.time.strftime('%b %e %R ').rjust(14),
+      file_data.name
     ]
   end
 end
--- 08.ls_object/lib/short_file_list.rb
+++ 08.ls_object/lib/short_file_list.rb
@@ -4,15 +4,15 @@ class ShortFileList
   COLUMNS = 3
   TEXT_WIDTH = 20
 
-  def initialize(file_list)
-    @file_list = file_list
+  def initialize(file_data_list)
+    @file_data_list = file_data_list
   end
 
   def show_list
-    formated_file_list = @file_list.map { |file| file.name.ljust(TEXT_WIDTH) }
-    rest_of_row = formated_file_list.size % COLUMNS
-    (COLUMNS - rest_of_row).times { formated_file_list.push(nil) } unless rest_of_row.zero?
-    columns = formated_file_list.each_slice(formated_file_list.size / COLUMNS).to_a
+    formated_file_data_list = @file_data_list.map { |file_data| file_data.name.ljust(TEXT_WIDTH) }
+    rest_of_row = formated_file_data_list.size % COLUMNS
+    (COLUMNS - rest_of_row).times { formated_file_data_list.push(nil) } unless rest_of_row.zero?
+    columns = formated_file_data_list.each_slice(formated_file_data_list.size / COLUMNS).to_a
     columns.transpose.map { |column| column.join.strip }
   end
 end
